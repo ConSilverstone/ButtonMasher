b45dbc150bee48f4e7ed047efc1542d19da038a1
ConSilverstone <ConnorJamesGallagher@outlook.com>
2018-09-05 15:41:54 +0100

    Added the gamewindow and the loop to run the game.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,20 @@
+//Included Libraries
+#include <SFML/Graphics.hpp>
+
 
 //start of program functions
 int main() 
 {
+	//declaring a type called gamewindow for SFML's render window 
+	sf::RenderWindow gamewindow;
+
+	while (true) //Game Loop
+	{
+		//TODO: Check for input
+
+		//TODO: Update game state
+
+		//TODO: Draw Graphics
+	}
 	return 0; //Used to end off the program
 }
